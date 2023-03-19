acd83ee89bfeb573b09f3129b4f6236f93056004
Ismeet Singh <ismeetsingh275@gmail.com>
2023-03-19 15:46:13 -0400

    initial project commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>An Amazing startup idea.</p>
+        <p>Assignment done.</p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
